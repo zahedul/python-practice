4f21220689ef5c5d568602790ecc3b466e74087f
Zahedul Alam <z.alam015@gmail.com>
2020-09-25 11:04:39 +0600

    working on regex with htmlparser

---
+++ hackerrank/html-parser-part-1.py
@@ -0,0 +1,22 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    
+    def handle_starttag(self, tag, attrs):
+        print(f'Start : {tag}')
+        for ele in attrs:
+            print (f'-> {ele[0]} > {ele[1]}')
+        
+    def handle_endtag(self, tag):
+        print(f'End   : {tag}')
+        
+    def handle_startendtag(self, tag, attrs):
+        print(f'Empty : {tag}')
+        for ele in attrs:
+            print (f'-> {ele[0]} > {ele[1]}')
+        
+
+parser = MyHTMLParser()
+
+parser.feed(''.join([input().strip() for _ in range(int(input()))]))
