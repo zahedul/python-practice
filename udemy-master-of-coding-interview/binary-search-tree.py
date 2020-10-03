fadd424215fad2933b942f8fc65852cb66ba2e72
Zahedul Alam <z.alam015@gmail.com>
2020-10-03 11:48:09 +0600

    working on udemy course

---
+++ udemy-master-of-coding-interview/binary-search-tree.py
@@ -0,0 +1,17 @@
+class BinarySearchTree:
+    
+    def __init__(self):
+        self.root = null;
+    
+    def insert(value):
+        pass
+
+    def lookup(value):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    # execute only if run as the entry point into the program
+    main()
