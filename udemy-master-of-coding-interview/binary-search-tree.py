2774b200eab221cc9156f990d1f90c8835800576
Zahedul Alam <z.alam015@gmail.com>
2020-10-03 12:09:09 +0600

    working on create binary search treee

--- udemy-master-of-coding-interview/binary-search-tree.py
+++ udemy-master-of-coding-interview/binary-search-tree.py
@@ -6,11 +6,30 @@ class BinarySearchTree:
     def __init__(self):
         self.root = null;
     
-    def insert(value):
+    def insert(self, value):
         new_node = Node(value)
-        pass
 
-    def lookup(value):
+        if self.root == null:
+            self.root = new_node
+        else:
+            current_node = self.root
+
+            while True:
+                if value < current_node.value: #left
+                    if not current_node.left:
+                        current_node.left = new_node
+                        return self
+                    current_node = current_node.left
+                else: #right
+                    if current_node.right:
+                        current_node.right = new_node
+                        return self
+                    
+                    current_node = current_node.right
+
+        
+
+    def lookup(self, value):
         pass
 
 def main():
