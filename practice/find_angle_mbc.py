42a081fac80fdc248abfcb28c1f64d6f1e3a564b
Zahedul Alam <z.alam015@gmail.com>
2020-11-13 23:39:27 +0600

    working on triangle quest 2

--- practice/find_angle_mbc.py
+++ practice/find_angle_mbc.py
@@ -5,4 +5,4 @@ bc = int(input())
 
 res = math.atan2(ab,bc)
 
-print(int(math.degrees(res)), chr(176), sep='')
+print(int(round(math.degrees(res))), chr(176), sep='')
---
+++ practice/triangle-quest-2.py
@@ -0,0 +1,3 @@
+n = int(input())
+
+[print(((10**i)//9)**2) for i in range(1, n+1)]
