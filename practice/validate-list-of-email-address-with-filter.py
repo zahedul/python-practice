67213f24cd453a28158add2e02a4b1258bb1212a
Zahedul Alam <z.alam015@gmail.com>
2020-11-14 00:42:37 +0600

    working on regex

---
+++ practice/validate-list-of-email-address-with-filter.py
@@ -0,0 +1,20 @@
+import re
+
+regex = '[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$'
+
+def fun(s):
+    # return True if s is a valid email, else return False
+    return re.match(regex, s)
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
